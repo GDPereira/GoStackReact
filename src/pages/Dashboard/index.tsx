443eec26dc940fa0182f78385dd865104aad1552
Guilherme Deluca <gdelucapereira@gmail.com>
2020-09-27 23:15:23 -0300

    Initial commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/69254749?s=460&u=8aa353e110f459df6133bddec1e47b6e10af727e&v=4"
+            alt="Guilherme Deluca"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/69254749?s=460&u=8aa353e110f459df6133bddec1e47b6e10af727e&v=4"
+            alt="Guilherme Deluca"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/69254749?s=460&u=8aa353e110f459df6133bddec1e47b6e10af727e&v=4"
+            alt="Guilherme Deluca"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+}
+
+export default Dashboard;
